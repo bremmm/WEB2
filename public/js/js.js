8d2cbf67904c7ae8e5be15d2a9e8c8c3ed74aab9
José Oávio <joabremm@hotmail.com>
2019-12-19 11:18:09 -0300

    "finalização" do experimento

--- public/js/js.js
+++ public/js/js.js
@@ -138,17 +138,17 @@ $(function(){
     });
 });
 
-// $(function(){
+$(function(){
 
-//     $("div.i").css("display", "none");
-//     $("div#i1").addClass("active");
+    $("div.b1").css("display", "none");
+    $("div#f1").addClass("active");
 
-//     $("a#menuI").on("click", function( e ){         
-//       e.preventDefault();
+    $("a#menuM").on("click", function( e ){         
+      e.preventDefault();
         
-//     	$("div").removeClass("active");
-//         var id = $(this).attr("href"); 
-//         $("#"+id+"").addClass("active");
+    	$("div").removeClass("active");
+        var id = $(this).attr("href"); 
+        $("#"+id+"").addClass("active");
         
-//     });
-// });
+    });
+});
